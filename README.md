214119594aa98ebbde8eb81552e03010b02f0321
Danny Bugingo <dbugingo@hotmail.com>
2019-12-19 16:50:22 -0800

    added support for consonant clusters
    also changed translation for words that begin with vowels (now in line with Standard Wikipedia Pig Latin)

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # oink
-pig latin translation
-igpay atinlay anslationtray
+ **pig latin translation** *\*oink\** **igpay atinlay anslationtray**
+
+
+
+stuff to do
+ 
+* [ ] deal with 'y' sometimes being a vowel
+ 
+* [ ] add English to Pig Latin translation
+
+* [ ] add a nice interface
+* [ ] discuss challenges of transcribing and translating a largely oral dialect with native Pig Latin speaker
+
+stuff to done
+ 
+* [x] make into a class, protect against numbers and punctuation (thanks Russell!)
+* [x] deal with consonant clusters
--- oink.py
+++ oink.py
@@ -2,47 +2,57 @@ import string
 
 class Oink(object):
     '''
-    your kid came home from summer camp and you can't understand them
+    your pig came home from summer camp and you can't understand them
     '''    
     @classmethod
     def translate(cls,input_string):
         '''
-        translate that crap your kid is saying into ENGLISH
+        translate that crap your pig is saying into ENGLISH
         '''
-        assert isinstance(input_string,str), "source must be a string"
+        assert isinstance(input_string, str), "source must be a string"
 
         input_string = input_string.strip().split(' ')
+        output_string = ""
+        
+        for word in input_string:
+            
+            if not word[0] in string.digits:
+            
+                if Oink.is_vowel(word[0]):
+                    output_string = output_string + " " + \
+                    (word + "way" if not word[-1] in string.punctuation \
+                     else word[:-1] + "way" + word[-1]) 
 
-        return ' '.join([
-            (
-                word[1:] + word[0]+"way" \
-                if not \
-                    word[-1] in string.punctuation \
-                else \
-                    word[1:-1] + word[0]+"way"+word[-1]
-            ) \
-            if \
-                Oink.is_vowel(word[0]) \
-            else (
-                (
-                    word[1:] + word[0]+"ay" \
-                    if not \
-                        word[-1] in string.punctuation \
-                    else \
-                        word[1:-1] + word[0]+"ay"+word[-1]
-                ) if not \
-                    word[0] in string.digits \
-                else \
-                    word \
-            ) \
-            for word in input_string \
-        ])
+                elif Oink.is_vowel(word[1]):
+                    output_string = output_string + " " + \
+                    (word[1:] + word[0] + "ay" if not word[-1] in string.punctuation \
+                     else word[1:-1] + word[0] + "ay" + word[-1])
 
+                # word begins with two consonants
+                elif Oink.is_vowel(word[2]):
+                    output_string = output_string + " " + \
+                    (word[2:] + word[0:2] + "ay" if not word[-1] in string.punctuation \
+                     else word[2:-1] + word[0:2] + "ay" + word[-1])
+                
+                # word begins with three consonants
+                elif Oink.is_vowel(word[3]):
+                    output_string = output_string + " " + \
+                    (word[3:] + word[0:3] + "ay" if not word[-1] in string.punctuation \
+                     else word[3:-1] + word[0:3] + "ay" + word[-1])
+                    
+                # I don't think any English words start with four consonants
+                # I also can't think of any words that might SEEM to start with four consonants that could throw off the system
+                else: output_string = output_string + " " + word
+                
+            else: output_string = output_string + " " + word 
+                
+        return output_string[1:]
+            
     @classmethod
-    def is_vowel(cls,letter):
+    def is_vowel(cls, letter):
         if letter.lower() in 'aeiou':
             return True
         return False
-        
-    def oink(self,source):
+
+    def oink(self, source):
         return Oink.translate(source)
